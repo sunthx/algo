799b40c894e1450b3fd664c235bf94b9d697481d
sunthx <sunth0101@126.com>
2018-10-20 22:55:54 +0800

    first commit

---
+++ c-sharp/Node.cs
@@ -0,0 +1,8 @@
+namespace c_sharp
+{
+    public class Node {
+        public string Value {get;set;}
+        public Node Next{get;set;}
+        public Node Previous{set;get;}
+    }
+}
--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -6,7 +6,7 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to algo! Come On,Bady!");
         }
     }
 }
